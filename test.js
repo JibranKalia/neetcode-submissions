2eeab7d321d108432b8710655444a05443d9c1db
Jibran Kalia <jibran.kalia@hey.com>
2026-06-02 16:34:48 -0500

    Replace two-sum-ii harness with three-sum draft
    Pivoting the working scratch file to the three-integer-sum (3Sum) problem.
    The two-sum-ii solution was complete; this sets up the next practice problem
    using a sort + two-pointer approach with an inner twoSum helper to eliminate
    one degree of freedom.

--- test.js
+++ test.js
@@ -1,37 +1,52 @@
 class Solution {
   /**
-   * @param {number[]} numbers
-   * @param {number} target
-   * @return {number[]}
+   * @param {number[]} nums
+   * @return {number[][]}
    */
-  twoSum(numbers, target) {
-    let left = 0;
-    let right = numbers.length - 1;
+  threeSum(nums) {
+    const n = nums.sort((a, b) => a - b);
+    const len = n.length;
+    const ret = [];
 
-    while (left < right) {
-      const sum = numbers[left] + numbers[right];
+    /**
+     * [-4, -1, -1, 0, 1, 2, 2]
+     * 
+     * - target and pass it to two sum. if not empty array add to res
+     * - duplicates?
+     *    Sorting will prevent
+     */
 
-      if (sum === target) {
-        return [numbers[left], numbers[right]];
-      } else if (sum < target) {
-        left++;
-      } else {
-        right--;
+    function twoSum(l, target) {
+      let left = l;
+      let right = len - 1;
+
+      while (left < right) {
+        const sum = n[left] + n[right];
+        if (sum === target) {
+          ret.push([-target, n[left], n[right]]);
+          left++;
+          right--;
+          while (left < right && n[left] === n[left - 1]) left++;
+          while (left < right && n[right] === n[right + 1]) right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
       }
     }
+
+    for (let i = 1; i < len - 1; i++) {
+      if (i > 1 && n[i - 2] === n[i - 1]) continue;
+      twoSum(i, -(n[i - 1]));
+    }
+
+    return ret;
   }
-
-
-  /**
-   * target = 13
-   * 
-   * [1, 2, 4, 8, 10, 13, 15]
-   */
 }
 
-const numbers = [2, 3, 4]
-const target = 6
+const n = [0, 0, 0, 0];
+const res = new Solution().threeSum(n)
 
-const res = new Solution().twoSum(numbers, target);
 console.log(res);
 
