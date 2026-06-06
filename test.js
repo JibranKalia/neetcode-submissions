3708620592c3dd9e9bb16de77b14c98ec06fbb50
Jibran Kalia <jibran.kalia@hey.com>
2026-06-05 21:39:50 -0500

    Replace trap-water solution with longest-substring draft in test.js
    test.js is a local scratch file used to develop and manually test solutions before submitting to NeetCode. The previous occupant (trapping rain water) has been submitted, so the file is now being repurposed for the next problem: longest substring without repeating characters.

--- test.js
+++ test.js
@@ -1,52 +1,41 @@
 class Solution {
   /**
-   * @param {number[]} height
+   * @param {string} s
    * @return {number}
    */
-  trap(height) {
-    const h = height;
-    const n = h.length;
+  lengthOfLongestSubstring(s) {
+    const hash = new Array(150).fill(0);
+    let l = 0;
+    let r = 1;
+    let maxSubstring = 1;
 
-    const maxR = Array(n).fill(0);
-    const maxL = Array(n).fill(0);
+    hash[s.charCodeAt(l)] = 1;
 
-    maxL[0] = h[0];
-    maxR[n - 1] = h[n - 1];
-
-    for (let i = 1; i < n; i++) {
-      maxL[i] = Math.max(maxL[i - 1], h[i]);
-    }
-
-    for (let j = n - 2; j > - 0; j--) {
-      maxR[j] = Math.max(h[j], maxR[j + 1]);
-    }
-
-    function volume(i) {
-      const minH = Math.min(maxR[i], maxL[i])
-      if (minH > 0) {
-        return minH - h[i]
+    while (r < s.length) {
+      console.log(l, r, s.charAt(l), s.charAt(r), maxSubstring);
+      if (hash[s.charCodeAt(r)] === 1) {
+        hash[s.charCodeAt(l)] = 0;
+        l++;
+      } else {
+        hash[s.charCodeAt(r)] = 1;
+        r++;
       }
-      return 0;
+      maxSubstring = Math.max(maxSubstring, r - l);
     }
 
-    let maxVol = 0;
-
-    for (let i = 0; i < n; i++) {
-      const vol = volume(i)
-      maxVol += vol;
-    }
-
-    return maxVol
+    return maxSubstring;
   }
 }
 
 
 
-const height = [4, 2, 0, 3, 2, 5]
+
+
+const s = "au"
 
 
 
-const res = new Solution().trap(height)
+const res = new Solution().lengthOfLongestSubstring(s)
 
 console.log(res);
 
