7b8436b658a7d20d316a2ad19cf63efe75c14118
Jibran Kalia <jibran.kalia@hey.com>
2026-06-09 07:35:51 -0500

    Replace longest-substring draft with characterReplacement skeleton in test.js
    Switching focus to the "Longest Repeating Character Replacement" problem (Sliding Window). The previous draft in test.js was a partially-working attempt at Longest Substring Without Repeating Characters; this replaces it with the new problem's scaffolding so the scratch file stays aligned with the current problem being worked through.

--- test.js
+++ test.js
@@ -1,41 +1,77 @@
 class Solution {
   /**
    * @param {string} s
+   * @param {number} k
    * @return {number}
    */
-  lengthOfLongestSubstring(s) {
-    const hash = new Array(150).fill(0);
+  characterReplacement(s, k) {
     let l = 0;
-    let r = 1;
-    let maxSubstring = 1;
+    let r = 0;
+    let localK = k;
+    let maxLen = 0;
 
-    hash[s.charCodeAt(l)] = 1;
+    while (l < s.length && r < s.length) {
+      maxLen = Math.max(r - l + 1, maxLen);
+      console.log(l, r, maxLen);
 
-    while (r < s.length) {
-      console.log(l, r, s.charAt(l), s.charAt(r), maxSubstring);
-      if (hash[s.charCodeAt(r)] === 1) {
-        hash[s.charCodeAt(l)] = 0;
-        l++;
-      } else {
-        hash[s.charCodeAt(r)] = 1;
+      if (s.charAt(l) === s.charAt(r)) {
         r++;
+      } else if (localK > 0) {
+        maxLen = Math.max(r - l + 1, maxLen);
+        localK--;
+        r++;
+      } else {
+        maxLen = Math.max(r - l, maxLen);
+        localK = k;
+        l++;
+        r = l;
       }
-      maxSubstring = Math.max(maxSubstring, r - l);
     }
-
-    return maxSubstring;
+    return maxLen;
   }
 }
 
+/**
+ *
+ * start l / r
+ *
+ * keep moving r when
+ *
+ * if (l == r)
+ *  ++
+ * if (l != r)
+ *  localK--
+ * else
+ *  window size = max
+ *  localL
+ *  while (localL == l)
+ *    l++
+ *    r = l
+ *
+ *
+ * XYYX
+ * XXXX - 4
+ *
+ * AAABABB
+ *
+ * AAABABB
+ *
+ *
+ */
 
 
 
+// const k = 2
+// const s = "AAAA"
+//
+const s = "ABBB"
+const k = 2
 
-const s = "au"
+// const s="XYYX"
+// const k=2
+//
 
+const res = new Solution().characterReplacement(s, k)
 
-
-const res = new Solution().lengthOfLongestSubstring(s)
-
-console.log(res);
+console.log('res', res);
 
