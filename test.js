ffb535b530f76817e8f9b9b86aecc2c5a45bc659
Jibran Kalia <jibran.kalia@hey.com>
2026-06-14 13:33:30 -0500

    Implement maxSlidingWindow

--- test.js
+++ test.js
@@ -1,45 +1,69 @@
 class Solution {
   /**
-   * @param {string} s
+   * @param {number[]} nums
    * @param {number} k
-   * @return {number}
+   * @return {number[]}
    */
-  numKLenSubstrNoRepeats(s, k) {
-    if (s.length < k) return 0;
-    const set = new Array(26).fill(0);
-    let totalNoRepeated = 0;
+  maxSlidingWindow(nums, k) {
+    const res = [];
+    const queue = new Array(nums.length).fill(null);
+    //intial value
+    queue[0] = 0;
 
-    function isValid() {
-      return set.every((c) => c <= 1);
-    }
+    let left = 0;
+    let right = 0;
 
-    function idx(i) {
-      return s.charCodeAt(i) - 'a'.charCodeAt(0);
-    }
-
-    for (let i = 0; i < k; i++) {
-      set[idx(i)] += 1;
-    }
-
-    if (isValid()) {
-      totalNoRepeated++;
-    }
-
-
-    for (let i = k; i < s.length; i++) {
-      set[idx(i)] += 1;
-      set[idx(i - k)] -= 1;
-      if (isValid()) {
-        totalNoRepeated++;
+    // build inital window
+    for (let i = 1; i < k; i++) {
+      while (left <= right && nums[queue[right]] < nums[i]) {
+        right--;
       }
+      right++;
+      queue[right] = i;
     }
 
-    return totalNoRepeated;
+    // push for inital window
+    res.push(nums[queue[left]]);
+
+    for (let i = k; i < nums.length; i++) {
+      const windowLeft = i - k + 1;
+      while (left <= right && queue[left] < windowLeft) {
+        left++;
+      }
+      while (left <= right && nums[queue[right]] < nums[i]) {
+        right--;
+      }
+      right++;
+      queue[right] = i;
+
+      res.push(nums[queue[left]]);
+    }
+
+    return res;
   }
+
+  /** 
+  
+  
+    initially just add [0]
+    then for check left if smaller then curr pop. Keep doing until hit bigger. Then push
+
+    Do that until index k - 1
+  
+    then for i = k till end of nums
+
+      check if left most idx in queue. if not remove.
+      then check queue[right most]. If current if current bigger remove. Keep doing this.
+      then insert current
+      record queue[left]
+  ;
+  */
 }
 
-const s = "havefunonneetcode"
-const k = 5
+// const nums = [1, -1]
+// const k = 1
+const nums = [1, 3, -1, -3, 5, 3, 6, 7]
+const k = 3
 
-const res = new Solution().numKLenSubstrNoRepeats(s, k);
+const res = new Solution().maxSlidingWindow(nums, k);
 console.log('res', res);
